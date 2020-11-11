ecb0546e1d559badb6506d79bf5295d76ddd6fd3
Richanshu Jha <=>
2020-11-11 12:49:06 -0500

    Removed print statements

--- solution.py
+++ solution.py
@@ -112,15 +112,15 @@ def ping(host, timeout=1):
         vars = ['0', '0.0', '0', '0.0']
         return(vars)
     
-    print("Pinging " + dest + " using Python:")
-    print("")
+    #print("Pinging " + dest + " using Python:")
+    #print("")
     # Calculate vars values and return them
     #  vars = [str(round(packet_min, 2)), str(round(packet_avg, 2)), str(round(packet_max, 2)),str(round(stdev(stdev_var), 2))]
     # Send ping requests to a server separated by approximately one second
     delays = []
     for i in range(0,4):
         delay = doOnePing(dest, timeout)*1000
-        print(delay)
+        #print(delay)
         delays.append(delay)
         time.sleep(1)  # one second
 
